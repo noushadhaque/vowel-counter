52d86d608d7c529f9d248a9f2c266824b26ef619
Noushad Haque <83365805+noushadhaque@users.noreply.github.com>
2024-04-17 21:16:12 +0600

    Delete case-convert.js

--- case-convert.js
+++
@@ -1,9 +0,0 @@
-const userInput = "Noushad"
-const searchUserInput = "noushad"
-
-if(userInput.toLocaleLowerCase() == searchUserInput){
-    console.log('User exist')
-}
-else{
-    console.log('User not exist')
-}
